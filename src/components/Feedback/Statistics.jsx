fe0d61f85f052718b73e2cde05257f9b8c5b8bc9
ArtemPluktus <artempluktus@gmail.com>
2023-06-15 22:37:37 +0200

    done

---
+++ src/components/Feedback/Statistics.jsx
@@ -0,0 +1,36 @@
+import css from './Feedback.module.css';
+
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+  feedbackGiven,
+  title,
+}) => {
+  return (
+    <div>
+      <h2 className={css.title}>{title}</h2>
+      {feedbackGiven ? (
+        <ul className={css.statsList}>
+          <li className={css.statsItem} name="good">
+            Good: {good}
+          </li>
+          <li className={css.statsItem} name="neutral">
+            Neutral: {neutral}
+          </li>
+          <li className={css.statsItem} name="bad">
+            Bad: {bad}
+          </li>
+          <li className={css.statsItem}>Total: {total}</li>
+          <li className={css.statsItem}>
+            Positive feedback: {positivePercentage}%.
+          </li>
+        </ul>
+      ) : (
+        <p>There is no feedback yet.</p>
+      )}
+    </div>
+  );
+};
